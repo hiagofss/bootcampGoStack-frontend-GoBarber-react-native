7b68f6d67bb0d259683902ce6dd064915889a25a
Hiago Souza <hiagofss98@gmail.com>
2019-08-05 12:04:14 -0300

    Configure workspace

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function App() {
+  return (
+    <View>
+      <h1>Gelo</h1>
+    </View>
+  );
+}
