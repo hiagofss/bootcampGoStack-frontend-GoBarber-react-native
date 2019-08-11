4cea62adb36abf6e28c993dba8ed921dbacd8934
Hiago Souza <hiagofss98@gmail.com>
2019-08-11 11:19:23 -0300

    Create Route to Dashboard

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
-import Routes from './routes';
+
+import App from './App';
 
 import './config/ReactotronConfig';
 
 import { store, persistor } from './store';
 
-export default function App() {
+export default function Index() {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-
-        <Routes />
+        <App />
       </PersistGate>
     </Provider>
   );
